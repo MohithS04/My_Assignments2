a19dc3d62bf11297e04bdb008bd5bb0909384a8f
MohithS04 <88337483+MohithS04@users.noreply.github.com>
2021-08-15 11:51:09 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+names = ["akash","mohit","ravi","varun"]
+print(names)
+
+names[1] = "rishi"       #replace
+print(names)
+
+names.insert(1,"vamshi")    #insert
+print(names)
+
+names.sort()                #sort
+print(names)
+
+del names[2]                #delete
+print(names)
+
+names.append("rahul")        #append
+print(names)
+
+names.reverse()              #reverse
+print(names)
+
+x = names.index("ravi")      #index
+print(x)
+
